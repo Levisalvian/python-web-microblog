0fbd576b6d309109c8c4ca82c7e81e429923ada3
Levisalvian <48515010+Levisalvian@users.noreply.github.com>
2023-06-02 17:19:34 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import datetime
+import os
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    app.db = client.microblog
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        print([e for e in app.db.entries.find({})])
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
+            app.db.entries.insert_one(
+                {"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(
+                    entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+
+        ]
+
+        for entry in entries_with_date:
+            print(entry[0], entry[1], entry[2])
+
+        return render_template("home.html", entries=entries_with_date)
+
+    return app
